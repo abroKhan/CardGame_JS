a79d9cd0f8418c12bcb1c88b6666ff41c7ab834a
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-13 12:07:37 +0500

    commit

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let hero = {
   name: "Wizard",
   avatar: "images/wizard.png",
   health: "70",
-  diceRoll: 5,
+  diceRoll: [5, 1, 3],
+  diceCount: 3,
 };
 
 let monster = {
@@ -12,19 +13,24 @@ let monster = {
   name: "Orc",
   avatar: "images/orc.png",
   health: "40",
-  diceRoll: 4,
+  diceRoll: [4],
+  diceCount: 1,
 };
 
 function renderCharacter(data) {
-  const { id, name, avatar, health, diceRoll } = data;
+  const { id, name, avatar, health, diceRoll, diceCount } = data;
+
+  let diceHtml = "";
+  for (let i = 0; i < diceCount; i++) {
+    diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
+  }
+
   document.getElementById(id).innerHTML = `
     <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}" />
         <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">
-        <div class="dice"> ${diceRoll} </div>
-        </div>
+        <div class="dice-container">${diceHtml}</div>
     </div>
 `;
 }
