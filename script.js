473aab59bc65d865b99de807a4923220a732f65e
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-13 11:52:11 +0500

    commit

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 // Hero Section
+let hero = {
+  id: "hero",
+  name: "Wizard",
+  avatar: "images/wizard.png",
+  health: "70",
+  diceRoll: 5,
+};
 
-// Monster Section
-document.getElementById("monster").innerHTML = `
+let monster = {
+  id: "monster",
+  name: "Orc",
+  avatar: "images/orc.png",
+  health: "40",
+  diceRoll: 4,
+};
+
+function renderCharacter(data) {
+  document.getElementById(data.Id).innerHTML = `
     <div class="character-card">
-        <h4 class="name">Orc</h4>
-        <img class="avatar" src="images/orc.png" />
-        <p class="health">health: <b> 10 </b></p>
+        <h4 class="name"> ${data.name} </h4>
+        <img class="avatar" src="${data.avatar}" />
+        <p class="health">health: <b> ${data.health} </b></p>
         <div class="dice-container">
-            <div class="dice">4</div>
-        </div>
-    </div>
-`;
-
-const heroId = "hero";
-const heroName = "Wizard";
-const heroImage = "images/wizard.png";
-const heroHealth = "6";
-const heroDiceRoll = 4;
-
-const monsterId = "monster";
-const monsterName = "Orc";
-const monsterImage = "images/orc.png";
-const monsterHealth = "10";
-const monsterDiceRoll = 2;
-
-function renderCharacter(elementId, name, avatar, health, diceRoll) {
-  document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">
-        <div class="dice"> ${diceRoll} </div>
+        <div class="dice"> ${data.diceRoll} </div>
         </div>
     </div>
 `;
 }
 
-renderCharacter(heroId, heroName, heroImage, heroHealth, heroDiceRoll);
-renderCharacter(monsterId, monsterName, monsterImage, monsterHealth, monsterDiceRoll);
+renderCharacter(hero);
+renderCharacter(monster);
+// renderCharacter(
+//   monsterId,
+//   monsterName,
+//   monsterImage,
+//   monsterHealth,
+//   monsterDiceRoll
+// );
