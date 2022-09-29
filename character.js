31a90dee4022f16d03fdfd0d1c71c802bde1ab8f
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-29 16:19:09 +0500

    commit

--- character.js
+++ character.js
@@ -1,7 +1,10 @@
-import { getDiceRollArray } from "./utils.js";
+import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
 function Character(data) {
   Object.assign(this, data);
+
+  this.diceArray = getDicePlaceholderHtml(this.diceCount);
+
   this.getDiceHtml = function (diceCount) {
     return getDiceRollArray(diceCount)
       .map(function (num) {
@@ -12,18 +15,17 @@ function Character(data) {
 
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
-    const diceHtml = this.getDiceHtml(diceCount);
-
+    let diceHtml = this.getDiceHtml(diceCount);
     return `
-        <div class="character-card">
-          <h4 class="name"> ${name} </h4>
-          <img class="avatar" src="${avatar}" />
-          <div class="health">health: <b> ${health} </b></div>
-          <div class="dice-container">    
-          ${diceHtml}
-          </div>
-        </div>`;
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${this.diceArray}
+                </div>
+            </div>`;
   };
 }
 
-export { Character };
+export default Character;
