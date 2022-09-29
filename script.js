c959fa1d478054764bed6aa2f39e34f64c02816e
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-29 16:10:46 +0500

    commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 import { characterData } from "./Data.js";
 import { Character } from "./character.js";
 
-
-function render() {
-  document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+function attack() {
+  render();
 }
 
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+}
+
+document.getElementById("attack-button").addEventListener("click", attack);
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 render();
