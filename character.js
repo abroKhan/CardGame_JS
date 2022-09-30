48c5e18401eae00f46de5e879a5ced6f7c35af2e
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-30 11:06:01 +0500

    commit

--- character.js
+++ character.js
@@ -1,9 +1,15 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
+const getPercentage = (remainingHealth, maximumHealth) => {
+  (100 * remainingHealth) / maximumHealth;
+};
+
 function Character(data) {
   Object.assign(this, data);
 
   this.diceArray = getDicePlaceholderHtml(this.diceCount);
+
+  this.maxHealth = this.health;
 
   this.getDiceHtml = function () {
     this.currentDiceScore = getDiceRollArray(this.diceCount);
@@ -13,9 +19,9 @@ function Character(data) {
   };
 
   this.takeDamage = function (attackScoreArray) {
-    const totalAttackScore = attackScoreArray.reduce((total, num) => {
-      return total + num;
-    });
+    const totalAttackScore = attackScoreArray.reduce(
+      (total, num) => total + num
+    );
     this.health -= totalAttackScore;
     if (this.health <= 0) {
       this.dead = true;
@@ -23,9 +29,19 @@ function Character(data) {
     }
   };
 
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth);
+    return `
+      <div class="health-bar-outer">
+          <div class="health-bar-inner *YOUR CODE HERE* " 
+          style="width: *YOUR CODE HERE* %;">
+          </div>
+      </div>`;
+  };
+
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount, diceArray } = this;
-    const diceHtml = this.getDiceHtml(diceCount);
+    const healthBar = this.getHealthBarHtml();
     return `
         <div class="character-card">
           <h4 class="name"> ${name} </h4>
